cceebd64cf36c4bec6265920a768e9d7c2497fab
fazm1 <farid.azzam.2001@gmail.com>
2024-12-24 13:29:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function question1( arr1 = new Array(-3, 8, 7, 6, 5, -4, 3, 2, 1)){
+    
+}
+
+function question2(){
+    
+    
+}
+
+function question3(){
+   
+}
+
+function question4(){
+   
+    
+    
+}
+
+
+var q1 = question1(3, -7, 2);
+var q2 = question2();
+var q3 = question3();
+var q4 = question4();
